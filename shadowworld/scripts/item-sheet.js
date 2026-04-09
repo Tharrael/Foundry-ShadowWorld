9b21abd2402e65ee9b099d5241a7b63f7ed2f507
Tharrael <thaienas@gmail.com>
2026-04-09 10:20:08 +0200

    Add async updateObject method and refine listeners

--- shadowworld/scripts/item-sheet.js
+++ shadowworld/scripts/item-sheet.js
@@ -2,12 +2,12 @@ export class ShadowWorldItemSheet extends ItemSheet {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-        classes: ["shadowworld", "sheet", "item"],
-        template: "systems/shadowworld/templates/item/item-sheet.hbs",
-        width: 400,
-        height: 300
-        });
-    }
+      classes: ["shadowworld", "sheet", "item"],
+      template: "systems/shadowworld/templates/item/item-sheet.hbs",
+      width: 400,
+      height: 300
+    });
+  }
 
   getData() {
     const context = super.getData();
@@ -15,10 +15,14 @@ export class ShadowWorldItemSheet extends ItemSheet {
     context.editable = this.isEditable;
     return context;
   }
-  activateListeners(html) {
-  super.activateListeners(html);
 
-    // 🔥 auto-submit při změně
-    html.find("input").change(ev => this._onSubmit(ev));
+  async _updateObject(event, formData) {
+    return this.object.update(formData);
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+    // Auto-submit on input change (optional - form submission will handle editors)
+    html.find("input[type='text'], input[type='number'], textarea").change(ev => this._onSubmit(ev));
   }
 }
